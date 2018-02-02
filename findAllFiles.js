0a3af766c83039c494e01169bf51db6b4c5932a8
inseok <inseok.ucla@gmail.com>
2018-02-02 09:42:52 -0800

    Add error handling for findAllFiles

--- findAllFiles.js
+++ findAllFiles.js
@@ -2,6 +2,9 @@ const path = require('path');
 const fs = require('fs');
 
 const findAllFiles = (directory, extension, filter) => {
+  if (!fs.existsSync(directory)) {
+    return 'The Folder Does Not Exist';
+  }
   const fileList = [];
   const recurse = subDirectory => {
     const files = fs.readdirSync(subDirectory);
@@ -11,16 +14,15 @@ const findAllFiles = (directory, extension, filter) => {
       const fileStat = fs.statSync(absoluteFilePath);
       if (fileStat.isDirectory()) {
         recurse(filePath);
-      } else {
-        if (file.endsWith(extension)) {
-          const data = fs.readFileSync(filePath, 'utf8');
-          if (data.indexOf(filter) >= 0) {
-            fileList.push(filePath);
-          }
+      } else if (file.endsWith(extension)) {
+        const data = fs.readFileSync(filePath, 'utf8');
+        if (data.indexOf(filter) >= 0) {
+          fileList.push(filePath);
         }
       }
     });
   };
+
   recurse(directory);
   return fileList;
 };
